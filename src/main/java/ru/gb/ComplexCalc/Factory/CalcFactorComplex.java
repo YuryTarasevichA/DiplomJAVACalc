dae4988778064f421df53882d80aa70bab08de54
Yura <yura.tarasevichunt@gmail.com>
2024-07-17 04:41:06 +0300

    gotov

--- src/main/java/ru/gb/ComplexCalc/Factory/CalcFactorComplex.java
+++ src/main/java/ru/gb/ComplexCalc/Factory/CalcFactorComplex.java
@@ -2,9 +2,21 @@ package ru.gb.ComplexCalc.Factory;
 
 
 import ru.gb.ComplexCalc.ComplexCalculate;
+/**
+ * Класс CalcFactorComplex реализует интерфейс CalcFactory и предоставляет
+ * метод create для создания объекта ComplexCalculate с заданными значениями действительной
+ * и мнимой частей комплексного числа.
+ */
 
 public class CalcFactorComplex implements CalcFactory{
-
+    /**
+     * Метод create создает и возвращает новый объект ComplexCalculate с заданными
+     * значениями действительной и мнимой частей комплексного числа.
+     *
+     * @param realPart действительная часть комплексного числа
+     * @param imaginaryPart мнимая часть комплексного числа
+     * @return объект ComplexCalculate с заданными значениями действительной и мнимой частей
+     */
     @Override
     public ComplexCalculate create(double realPart, double imaginaryPart) {
         return new ComplexCalculate(realPart, imaginaryPart);
--- src/main/java/ru/gb/ComplexCalc/ViewComplex.java
+++ src/main/java/ru/gb/ComplexCalc/ViewComplex.java
@@ -4,6 +4,7 @@ package ru.gb.ComplexCalc;
 
 import ru.gb.ComplexCalc.Factory.CalcFactory;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ViewComplex {
@@ -23,47 +24,49 @@ public class ViewComplex {
             System.out.println("1. Посчитать");
             System.out.println("0. Выйти");
             choise = scanner.nextInt();
-            switch (choise) {
-                case 1:
-                    while (true) {
-                        double realPart = promptDouble("Введите действительную часть комплексного числа: ");
-                        double imaginaryPart = promptDouble("Введите мнимую часть комплексного числа: ");
-                        ComplexCalc complexCalculate =
-                                calcFactoryComplex.create(realPart, imaginaryPart);
-                        double real = promptDouble("Введите действительную часть второго комплексного числа: ");
-                        double imaginary = promptDouble("Введите мнимую часть второго комплексного числа: ");
-                        complex = new ComplexCalculate(real, imaginary);
-                        String cmd = prompt("Выбирете действие: +, -, *, / :  ");
-                        if (cmd.equals("+")) {
-                            result = (ComplexCalculate) complexCalculate.add(complex);
-                            System.out.println(result.toString());
-                            break;
+            try {
+                switch (choise) {
+                    case 1:
+                        while (true) {
+                            double realPart = promptDouble("Введите действительную часть комплексного числа: ");
+                            double imaginaryPart = promptDouble("Введите мнимую часть комплексного числа: ");
+                            ComplexCalc complexCalculate = calcFactoryComplex.create(realPart, imaginaryPart);
+                            double real = promptDouble("Введите действительную часть второго комплексного числа: ");
+                            double imaginary = promptDouble("Введите мнимую часть второго комплексного числа: ");
+                            complex = new ComplexCalculate(real, imaginary);
+                            String cmd = prompt("Выбирете действие: +, -, *, / :  ");
+                            if (cmd.equals("+")) {
+                                result = (ComplexCalculate) complexCalculate.add(complex);
+                                System.out.println(result.toString());
+                                break;
+                            } else if (cmd.equals("-")){
+                                result = (ComplexCalculate) complexCalculate.subtract(complex);
+                                System.out.println(result.toString());
+                                break;
+                            } else if (cmd.equals("*")){
+                                result = (ComplexCalculate) complexCalculate.multiply(complex);
+                                System.out.println(result.toString());
+                                break;
+                            } else if (cmd.equals("/")){
+                                result = (ComplexCalculate) complexCalculate.divide(complex);
+                                System.out.println(result.toString());
+                                break;
+                            } else {
+                                System.out.println("Неверная операция. Пожалуйста, попробуйте еще раз.");
+                            }
                         }
-                        if (cmd.equals("-")){
-                            result = (ComplexCalculate) complexCalculate.subtract(complex);
-                            System.out.println(result.toString());
-                            break;
-                        }
-                        if (cmd.equals("*")){
-                            result = (ComplexCalculate) complexCalculate.multiply(complex);
-                            System.out.println(result.toString());
-                            break;
-                        }
-                        if (cmd.equals("/")){
-                            result = (ComplexCalculate) complexCalculate.divide(complex);
-                            System.out.println(result.toString());
-                            break;
-                        }
-                    }
-                case 0:
-                    System.out.println("Выход из калькулятора комплексных чисел");
-                    break;
-                default:
-                    System.out.println("Неверный выбор. Пожалуйста, попробуйте еще раз.");
+                        break;
+                    case 0:
+                        System.out.println("Выход из калькулятора комплексных чисел");
+                        break;
+                    default:
+                        System.out.println("Неверный выбор. Пожалуйста, попробуйте еще раз.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка: введено некорректное значение. Пожалуйста, введите целое число.");
+                scanner.next(); // очистка буфера сканера
             }
         } while (choise != 0);
-
-
     }
     private String prompt(String message) {
         Scanner in = new Scanner(System.in);
